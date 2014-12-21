29a62fb7b4e20577ecb9a3a45b46ceaa31fae1cd
bobbywhang <robertwhang@gmail.com>
2014-12-20 17:42:33 -0800

    add panels

--- public/feed/feed.js
+++ public/feed/feed.js
@@ -3,11 +3,20 @@ resourceApp.controller('feedController', function($http, $scope){
   $http.get('/api/v1/feed').success(function(resources){
     $scope.feed = resources; 
   });
+  $scope.submitForm = function(){
+  	$http.post('/api/v1/user', $scope.user).success(function(user){
+
+  		$http.post('/api/v1/user/'+user._id+'/resources', $scope.resource).success(function(rsc){
+  			$scope.feed.push(rsc);
+  		})
+  	})
+  }
 });
 
-resourceApp.config(function($stateProvider){
+resourceApp.config(function($stateProvider, $locationProvider){
+	$locationProvider.html5Mode(true);
   $stateProvider.state('feed' , {
-    url: '/feed' ,
+    url: '/' ,
     templateUrl: '/feed/feed.html',
     controller: 'feedController'
   
